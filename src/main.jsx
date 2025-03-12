78d3f207510185d5243b824529a46a8b3366c9fa
jaimezzapata <zapataval2304@gmail.com>
2025-03-12 16:11:36 -0500

    Introducción a las rutas

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import PanelControl from './pages/PanelControl'
 import './index.css'
-
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './pages/auth/Login'
+let router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />
+  },
+  {
+    path: '/panel',
+    element: <PanelControl />
+  },
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <PanelControl />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
